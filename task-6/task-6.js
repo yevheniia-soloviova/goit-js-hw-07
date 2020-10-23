f6140a394b00db8550ddf67c85c67392e31bd18d
Yevheniia Soloviova <65976186+yevheniia-soloviova@users.noreply.github.com>
2020-10-23 18:09:34 +0300

    Update task-6.js

--- task-6/task-6.js
+++ task-6/task-6.js
@@ -1,14 +1,18 @@
 const validationInput = document.querySelector("#validation-input");
 
 function inputFocusChange() {
-  if (validationInput.value.length < validationInput.dataset.length) {
+  if (validationInput.value.length == validationInput.dataset.length) {
     validationInput.classList.add("valid");
     validationInput.classList.remove("invalid");
-  } else {
+  }
+  if (validationInput.value.length != validationInput.dataset.length) {
     validationInput.classList.add("invalid");
     validationInput.classList.remove("valid");
   }
+  if (validationInput.value.length == 0) {
+    validationInput.classList.remove("invalid");
+  }
+  
 }
-
 validationInput.addEventListener("blur", inputFocusChange);
 
